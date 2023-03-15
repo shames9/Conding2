7962c1fc241b57ff912f6228f9d614ac56b8627f
shames9 <119880118+shames9@users.noreply.github.com>
2023-03-15 15:11:22 +0000

    Add files via upload

---
+++ WeeklyLab Work/Week2-myMaxilmilianProject/src/ofApp.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxMaxim.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+    void setup() override;
+    void update() override;
+    void draw() override;
+
+    void keyPressed(int key) override;
+    void keyReleased(int key) override;
+    void mouseMoved(int x, int y) override;
+    void mouseDragged(int x, int y, int button) override;
+    void mousePressed(int x, int y, int button) override;
+    void mouseReleased(int x, int y, int button) override;
+    void mouseEntered(int x, int y) override;
+    void mouseExited(int x, int y) override;
+    void windowResized(int w, int h) override;
+    void dragEvent(ofDragInfo dragInfo) override;
+    void gotMessage(ofMessage msg) override;
+
+    // For drawing
+    float waveform[4096]; //make this bigger, just in case
+    int waveIndex;
+    
+    ofSoundStream soundStream;
+
+    /* ofxMaxi*/
+    void audioIn(ofSoundBuffer& input) override; // not used in this example
+    void audioOut(ofSoundBuffer& output) override;
+    maxiOsc myOsc1;  //Statement myOsc1
+    maxiOsc myOsc2;  //Statement myOsc2
+    maxiOsc myOsc3;  //Statement myOsc3
+    maxiOsc myOsc4;  //Statement myOsc4
+    maxiOsc myOsc5;  //Statement myOsc5
+};
